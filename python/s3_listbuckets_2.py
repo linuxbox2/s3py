22e5a8e4ad2b7eb3c2b7987c5e3882534756708e
Matt Benjamin <mbenjamin@redhat.com>
2015-10-04 12:54:30 -0400

    s3_listbuckets_2.py: list all by default, don't print empty object count
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>

---
+++ python/s3_listbuckets_2.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+import boto
+import boto.s3.connection
+from boto.s3.key import Key
+import os
+
+rgw_host = os.environ['RGW_HOST']
+access_key = os.environ['RGW_ACCESS_KEY']
+secret_key = os.environ['RGW_SECRET_KEY']
+
+stop_at_one = False
+
+conn = boto.connect_s3(
+aws_access_key_id = access_key,
+aws_secret_access_key = secret_key,
+host = rgw_host,
+is_secure=False,
+calling_format = boto.s3.connection.OrdinaryCallingFormat(),
+)
+
+nbuckets = 0
+
+for bucket in conn.get_all_buckets():
+        nbuckets += 1
+        if stop_at_one and nbuckets > 1:
+                break
+        print "bucket %s" % bucket.name
+
+print "bucket count: %d total" % (nbuckets)
