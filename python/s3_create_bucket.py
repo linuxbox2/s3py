e9dadbdd4b14761a4bfac02b55d754ffa4c75690
Matt Benjamin <mbenjamin@redhat.com>
2015-10-23 12:56:25 -0400

    add create, delete bucket progs
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>

---
+++ python/s3_create_bucket.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import boto
+import boto.s3.connection
+from boto.s3.key import Key
+import os
+
+rgw_host = os.environ['RGW_HOST']
+access_key = os.environ['RGW_ACCESS_KEY']
+secret_key = os.environ['RGW_SECRET_KEY']
+
+bucket_name = "sorry_dave"
+
+conn = boto.connect_s3(
+aws_access_key_id = access_key,
+aws_secret_access_key = secret_key,
+host = rgw_host,
+is_secure=False,
+calling_format = boto.s3.connection.OrdinaryCallingFormat(),
+)
+
+# create it
+bucket = conn.create_bucket(bucket_name)
