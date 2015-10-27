786598e829b72641a4a229b94e50464c6ff25492
Matt Benjamin <mbenjamin@redhat.com>
2015-10-27 09:41:15 -0400

    fix s3_list1.py
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>

--- python/s3_list1.py
+++ python/s3_list1.py
@@ -2,17 +2,25 @@
 
 import boto
 import boto.s3.connection
-access_key = "C4B4D3E4H355VTDTQXRF"
-secret_key = "NRBkhM2rUZNUbydD86HpNJ110VpQjVroumCOHJXw"
+from boto.s3.key import Key
+import os
+
+rgw_host = os.environ['RGW_HOST']
+access_key = os.environ['RGW_ACCESS_KEY']
+secret_key = os.environ['RGW_SECRET_KEY']
+
 conn = boto.connect_s3(
 aws_access_key_id = access_key,
 aws_secret_access_key = secret_key,
-host = 'serpent',
+host = rgw_host,
 is_secure=False,
 calling_format = boto.s3.connection.OrdinaryCallingFormat(),
 )
-for bucket in conn.get_all_buckets():
-        print "{name}\t{created}".format(
-                name = bucket.name,
-                created = bucket.creation_date,
-)
+
+nobjects = 0
+
+bucket = conn.get_bucket('sorry_dave')
+print "bucket %s" % bucket.name
+for o in bucket.list():
+        print "obj: %s" % (o.key)
+        nobjects += 1
