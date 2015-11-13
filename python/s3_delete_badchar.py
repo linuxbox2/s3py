9d1b858ffcac050c8c080a82f8211c08600565df
Matt Benjamin <mbenjamin@redhat.com>
2015-11-13 10:55:17 -0500

    add deletes for long and badchar keys
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>

---
+++ python/s3_delete_badchar.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+import boto
+import boto.s3.connection
+from boto.s3.key import Key
+import os
+
+rgw_host = os.environ['RGW_HOST']
+access_key = os.environ['RGW_ACCESS_KEY']
+secret_key = os.environ['RGW_SECRET_KEY']
+
+conn = boto.connect_s3(
+aws_access_key_id = access_key,
+aws_secret_access_key = secret_key,
+host = rgw_host,
+is_secure=False,
+calling_format = boto.s3.connection.OrdinaryCallingFormat(),
+)
+
+b = conn.get_bucket('sorry_dave')
+k = Key(b)
+k.key = 'pppppppppppppppppppppppppppppppppppppppppppppppppppppppppppp/dddddddddddddddddddddddddddddddddddddddddddddddddd/iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy'
+k.delete()
+
