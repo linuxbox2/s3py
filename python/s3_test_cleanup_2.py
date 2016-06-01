b01822b304d3a6f79dd9344099b4c989a9b8af05
Matt Benjamin <mbenjamin@redhat.com>
2016-06-01 14:40:09 -0400

    update with port info
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>

--- python/s3_test_cleanup_2.py
+++ python/s3_test_cleanup_2.py
@@ -6,6 +6,7 @@ import boto.s3.connection
 from boto.s3.key import Key
 
 rgw_host = os.environ['RGW_HOST']
+rgw_port = int(os.environ['RGW_PORT'])
 access_key = os.environ['RGW_ACCESS_KEY']
 secret_key = os.environ['RGW_SECRET_KEY']
 
@@ -13,6 +14,7 @@ conn = boto.connect_s3(
 aws_access_key_id = access_key,
 aws_secret_access_key = secret_key,
 host = rgw_host,
+port = rgw_port,
 is_secure=False,
 calling_format = boto.s3.connection.OrdinaryCallingFormat(),
 )
